6500c80978008e80f67a2be5c37234e1c0ff552e
subham <subham.sengupta7@gmail.com>
2018-04-12 23:42:44 +0530

    change

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var app = express();
 var router = express.Router();
 var port = process.env.PORT || 8080;
 
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
